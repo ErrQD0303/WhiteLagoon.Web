5ef45517f5e64c99b19ce5b85515af206bb59aa7
Quốc Đạt Nguyễn <datvipcrvn@gmail.com>
2023-12-05 17:25:37 +0700

    05/12/2023

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //Atrribute to identifies an action to support HTTP Post method
+        public IActionResult Create(Villa obj)
+        {
+            //Custom Model Validation
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj); //write a script to create the Villa object in the database
+                _db.SaveChanges(); //Go into the database and create the Villa object
+                TempData["success"] = "The villa has been created successfully.";
+                return RedirectToAction("Index"); //Redirect to the Index action in the same controller
+                                                  //return RedirectToAction("Index", "Villa"); //Redirecto the Index action the Villa Controller
+            }
+            TempData["error"] = "The villa could not be created.";
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            /*Villa? obj = _db.Villas.Find(villaId);
+            var VillaList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 0);*/
+            if (obj == null)
+            {
+                /*return NotFound();*/
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost] //Atrribute to identifies an action to support HTTP Post method
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj); //write a script to update the Villa object in the database
+                _db.SaveChanges(); //Go into the database and update the Villa object
+                TempData["success"] = "The villa has been updated successfully.";
+                return RedirectToAction("Index"); //Redirect to the Index action in the same controller
+                                                  //return RedirectToAction("Index", "Villa"); //Redirecto the Index action the Villa Controller
+            }
+            TempData["error"] = "The villa could not be updated.";
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            /*Villa? obj = _db.Villas.Find(villaId);
+            var VillaList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 0);*/
+            if (obj == null)
+            {
+                /*return NotFound();*/
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost] //Atrribute to identifies an action to support HTTP Post method
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb); //write a script to delete the Villa object in the database
+                _db.SaveChanges(); //Go into the database and delete the Villa object
+                TempData["success"] = "The villa has been deleted successfully.";
+                return RedirectToAction("Index"); //Redirect to the Index action in the same controller
+                                                  //return RedirectToAction("Index", "Villa"); //Redirecto the Index action the Villa Controller
+            }
+            TempData["error"] = "The villa could not be deleted.";
+            return View();
+        }
+    }
+}
