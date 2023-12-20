bb06ce2e81a624280a28b9a4f9c9fbc2e4094e8e
ErrQD0303 <quocdat3396@gmail.com>
2023-12-21 01:27:26 +0700

    (1:26 GMT+7 12/21/2023) - Adding Radial Chart and customize its action

---
+++ WhiteLagoon.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    loadTotalBookingRadialChart();
+});
+
+function loadTotalBookingRadialChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: "/Dashboard/GetTotalBookingRadialChartData", //Link to Json data 
+        //returned by DashBoard/ GetTotalBookingRadialChartData endpoint
+        type: 'GET', //GET is the default HTTP method of action
+        dataType: 'json',
+        success: function (data) {
+            document
+                .querySelector("#spanTotalBookingCount")
+                .innerHTML = data.totalCount;
+
+            var sectionCurrentCount = document.createElement("span");
+            if (data.hasRatioIncreased) {
+                sectionCurrentCount.className = "text-success me-1";
+                sectionCurrentCount.innerHTML
+                    = '<i class="bi bi-arrow-up-right-circle me-1"></i> </span> ' + data.countInCurrentMonth;
+            }
+            else {
+                sectionCurrentCount.className = "text-danger me-1";
+                sectionCurrentCount.innerHTML = '<i class="bi bi-arrow-down-right-circle me-1></i> </span> ' + data.countInCurrentMonth;
+            }
+
+            document.querySelector("#sectionBookingCount").append(sectionCurrentCount);
+            document.querySelector("#sectionBookingCount").append("since last month");
+            /*document.querySelector("#sectionBookingCount").innerHTML = sectionCurrentCount.outerHTML + " since last month";*/
+
+            loadRadialBarChart("totalBookingsRadialChart", data);
+
+            $(".chart-spinner").hide();
+        }
+    });
+}
